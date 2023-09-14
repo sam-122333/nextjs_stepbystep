ef468255292d61d3579a2cf27ef16a710c591b8e
sam-122333 <106857494+sam-122333@users.noreply.github.com>
2023-09-14 09:31:13 +0530

    part 2

---
+++ part1/src/app/about/[...not-found]/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../page.module.css";
+
+const page = () => {
+  return (
+    <div className={styles.main}>
+      <h1>this about page 404 not available</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ part1/src/app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const NotFound = () => {
+  return (
+    <div className={styles.main}>
+      <h1>this is manual 404 not found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
